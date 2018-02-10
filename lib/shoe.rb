ca3b648588204d6947430b222ced0b6ed4283523
Chely Ho <adept_inept@yahoo.com>
2018-02-10 18:31:26 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,13 @@
 # Make your shoe class here!
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
   def initialize(title)
     @title = title
   end
 
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
